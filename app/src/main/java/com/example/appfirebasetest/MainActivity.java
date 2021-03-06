deda5eea41140b2e11d67c8959a145892466fbe3
Kayro Pereira <kayrofellyxbr@gmail.com>
2021-03-05 23:52:47 -0300

    Implementado Fragments a interface gráfica

--- app/src/main/java/com/example/appfirebasetest/MainActivity.java
+++ app/src/main/java/com/example/appfirebasetest/MainActivity.java
@@ -7,15 +7,10 @@ import android.view.View;
 import android.view.Menu;
 import android.widget.Toast;
 
-import com.beardedhen.androidbootstrap.BootstrapButton;
-import com.beardedhen.androidbootstrap.BootstrapEditText;
-import com.example.appfirebasetest.model.dataUser;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
@@ -30,14 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
-    private BootstrapEditText edtNome, edtIdade;
-    private BootstrapButton btnCadastrar;
-
     private FirebaseAuth mAuth;
-
-    // Write a message to the database
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize Firebase
         mAuth = FirebaseAuth.getInstance();
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("friend");
-
-        edtNome = (BootstrapEditText) findViewById(R.id.edtNome);
-        edtIdade = (BootstrapEditText) findViewById(R.id.edtIdade);
-        btnCadastrar = (BootstrapButton) findViewById(R.id.btnCadastrar);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -74,14 +56,9 @@ public class MainActivity extends AppCompatActivity {
                 .setDrawerLayout(drawer)
                 .build();
 
-        btnCadastrar.setOnClickListener(e -> {
-            String key = myRef.push().getKey();
-            myRef.child(key).setValue(new dataUser(edtNome.getText().toString(), Integer.parseInt(edtIdade.getText().toString())));
-        });
-
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-//        NavigationUI.setupWithNavController(navigationView, navController);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
@@ -111,10 +88,10 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    //    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-//                || super.onSupportNavigateUp();
-//    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
